fef0a1b0ffbec8ffcc720e101da69661c84259bd
adrianramos97 <103299294+adrianramos97@users.noreply.github.com>
2023-04-10 20:46:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
